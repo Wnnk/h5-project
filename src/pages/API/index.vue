9c6992f809f00eb9d0ad529992a414c91b92aec4
Wnnk <1275056222@.com>
2024-10-24 22:14:54 +0800

    feat(custom): canvas轮盘

--- src/pages/API/index.vue
+++ src/pages/API/index.vue
@@ -1,9 +1,60 @@
 <template>
-  <view>api</view>
+  <div class="canvas-wrapper">
+    <canvas
+      id="myCanvas"
+      style="width: 300px; height: 200px"
+      ref="canvas"
+      canvas-id="myCanvas"
+    ></canvas>
+  </div>
+  <div>test</div>
 </template>
 
 <script setup>
+  import { ref } from 'vue'
+  let ctx = null
   onMounted(() => {
-    console.log('mounted')
+    ctx = uni.createCanvasContext('myCanvas')
+    drawBall(100, 100, 100, 0, 98, 'red')
+
   })
+
+  /**
+   * @description 背景圆
+   * @param {Number} beginPointX 开始坐标x
+   * @param {Number} beginPointY 开始坐标y
+   * @param {Number} radius 半径
+   * @param {Number} startAngle 开始角度
+   * @param {Number} endAngle 结束角度
+   * @param {String} backgroundColor 背景颜色
+   */
+  const drawBall = (beginPointX, beginPointY, radius, startAngle, endAngle, backgroundColor) => {
+    ctx.setStrokeStyle(backgroundColor)
+    ctx.setLineWidth(2)
+    ctx.arc(beginPointX, beginPointY, radius, startAngle, endAngle)
+    ctx.stroke()
+    ctx.setFillStyle(backgroundColor)
+    ctx.fill()
+    ctx.draw()
+  }
+
+  const drawRoundImage = (beginPointX, beginPointY, radius, startAngle, endAngle, image) => {
+    const temp = Math.min(image.width, image.height)
+    const scale = radius / temp
+    ctx.rect(
+      beginPointX - image.width * scale,
+      beginPointY - image.height * scale,
+      beginPointX + image.width * scale,
+      beginPointY + image.height * scale
+    )
+    ctx.drawImage(
+      image,
+      beginPointX - (image.width * scale) / 2,
+      beginPointY - (image.height * scale) / 2,
+      image.width * scale,
+      image.height * scale
+    )
+  }
 </script>
+
+<style></style>
--- src/pages/layout/index.vue
+++ src/pages/layout/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <view id="box"></view>
+  <view id="box" ref="box"></view>
 </template>
 
 <script setup>
+  import { ref, onMounted } from 'vue'
+  const box = ref(null)
+  const colWidth = 180
+  const items = getItems(30)
+
   onMounted(() => {
     const items = getItems(10)
-    console.log(items)
+    console.log(box.value.clientWidth)
+    init()
   })
   function getItems(len) {
     const items = []
@@ -40,12 +46,64 @@
     }
     return items
   }
-
+  /**
+    @description:创建图片
+    @param {Array} items:图片信息数组
+    @param {Number} imgWidth:每张图片的宽度
+  */
   const createImgs = (items, imgWidth) => {
-    for (let i = 0; i < array.length; i++) {
-      const element = array[i];
-      
+    for (let i = 0; i < items.length; i++) {
+      const { url, width: w, height: h } = items[i]
+      const img = document.createElement('img')
+      const ratio = imgWidth / w
+      const imgHeight = Math.floor(h * ratio)
+      img.width = imgWidth
+      img.height = imgHeight
+      img.src = url
+      box.value.appendChild(img)
     }
+  }
+
+  /**
+   * @description:计算列数，以及每列的间隙
+   * @param {Number} imgWidth:每张图片的宽度
+   * @**/
+  const getColumnInfo = (imgWidth) => {
+    console.log(box.value)
+    const boxWidth = box.value.clientWidth
+    const colNumber = Math.floor(boxWidth / imgWidth)
+
+    const spaceNumber = colNumber - 1
+    const leftSpace = boxWidth - colNumber * imgWidth
+    const colSpace = parseFloat((leftSpace / spaceNumber).toFixed(2, 10))
+    return { colNumber, colSpace }
+  }
+  /**
+   *  @description: 图片布局
+   *  @param {Array} items:图片信息数组
+   *  @param {Number} imgWidth:每张图片的宽度
+   *  @param {Number} rowSpace:每行的间隙
+   *  @param {Number} colSpace:每列的间隙
+   */
+  const setPosition = (items, colNumber, colSpace, rowSpace) => {
+    const nextTops = new Array(colNumber).fill(0)
+    for (let i = 0; i < items.length; i++) {
+      const img = box.value.children[i]
+      const top = Math.min.apply(null, nextTops)
+      img.style.top = `${top}px`
+      const index = nextTops.indexOf(top)
+      nextTops[index] = nextTopsp[index] + img.height + rowSpace
+      const left = index * colSpace + index * img.width
+      img.style.left = `${left}px`
+    }
+    const max = Math.max.apply(null, nextTops)
+    box.value.style.height = `${max}px`
+  }
+
+  const init = () => {
+    const { colNumber: cNumber, colSpace: cSpace } = getColumnInfo(colWidth)
+    const rSpace = cSpace
+    setPosition(items, cNumber, cSpace, rSpace)
   }
 </script>
 
